5e5e9e5377854fb3b1fec7e5f92e7e9cde12c2d4
tiennv0608 <tiennv0608@gmail.com>
2021-05-20 20:59:31 +0700

    b2

--- 00_Baitapthem/src/Main.java
+++ 00_Baitapthem/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println(list.searchByName("Hiệu"));
         list.deleteByName("Như Anh");
         list.display();
-
+        list.sortByName();
+        list.display();
     }
 }
