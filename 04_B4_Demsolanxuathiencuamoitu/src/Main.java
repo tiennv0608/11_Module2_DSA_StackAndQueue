c1f5cdc9cda94c2166b2f7756a49dd5433cb19db
tiennv0608 <tiennv0608@gmail.com>
2021-05-23 21:52:50 +0700

    b4

---
+++ 04_B4_Demsolanxuathiencuamoitu/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chuỗi muốn kiểm tra các từ lặp lại: ");
+        String string = scanner.nextLine();
+
+        // Tách các từ trong chuỗi vừa nhập
+        String[] listChart = string.split(" +");
+
+        // Tạo TreeMap
+        TreeMap<String, Integer> map = new TreeMap<>();
+        for (String str : listChart) {
+            str = str.toLowerCase();
+            if (map.containsKey(str)){
+                int plus = map.get(str)+1;
+                map.put(str, plus);
+            }else{
+                map.put(str, 1);
+            }
+        }
+        map.forEach((s, a)->{
+            System.out.println(s + ": " + a);
+        });
+    }
+}
