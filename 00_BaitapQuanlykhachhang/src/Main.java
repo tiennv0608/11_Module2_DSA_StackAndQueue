fb80eaa2c6feec83058737577d4c1bcdc2411437
tiennv0608 <tiennv0608@gmail.com>
2021-05-25 17:07:53 +0700

    Finish

--- 00_BaitapQuanlykhachhang/src/Main.java
+++ 00_BaitapQuanlykhachhang/src/Main.java
@@ -74,6 +74,7 @@ public class Main {
                     if (customer == null) {
                         System.out.println("No information");
                     } else {
+                        System.out.println(customer);
                         editingMenu();
                         option = scanner.nextInt();
                         scanner.nextLine();
